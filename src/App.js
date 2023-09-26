18021c0d00b36bf8d5fa6b7d8cf89a06ae59a377
Saran1612 <saran.senguttuvan@absyz.com>
2023-09-26 21:26:31 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dummy from './Pages/Home/Dummy';
 
 
 function App() {
   return (
     <div>
-      {/* <Router>
+      <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="home" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-      </Router> */}
-
-      <Dummy/>
+      </Router>
     </div>
   );
 }
